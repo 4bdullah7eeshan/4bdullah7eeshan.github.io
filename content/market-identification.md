d8994e200322caa7204c8f0cfb755401024f8bf2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-07-30 06:46:40 +0530

    Quartz sync: Jul 30, 2025, 6:46 AM

---
+++ content/market-identification.md
@@ -0,0 +1,6 @@
+---
+title: Market Identification
+---
+
+**Market Identification** is the process of identifying *where* our customers are. We essentially, decide *where* to look for them.
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Quintessential",
-        body: "Playfair Display",
+        header: "Alegreya",
+        body: "Alegreya Sans",
         code: "IBM Plex Mono",
       },
       colors: {
